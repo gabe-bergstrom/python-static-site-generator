0075e739ca693d2238a918e89e190ea2ad355dcb
gabe-bergstrom <gabe.bergstrom@reeher.com>
2020-11-19 11:37:11 -0600

    parser

---
+++ ssg/parsers.py
@@ -0,0 +1,36 @@
+
+import shutil
+from typing import List
+from pathlib import Path
+
+class Parser: 
+	extensions: List[str] = []
+	def __init__(self, extensions):
+		self.extensions = extensions
+
+	def valid_extension(extension, self):
+		return extension in self.extensions
+
+	def parse(self, path: Path, source: Path, dest:Path):
+		raise NotImplementedError
+	
+	def read(path):
+		with open(path, "r") as file
+			return file.read
+
+	def write(self, path, dest, content, ext=".html"):
+		full_path = self.dest / with_suffix(ext).name
+		with open(full_path, "w") as file:
+			file.write(content)
+
+	def copy(self, path, source, dest):
+		shutil.copy2(path, dest / path.relative_to(source))
+
+class ResourceParser(Parser):
+	extensions = [".jpg", ".png", ".gif", ".css", ".html"]
+
+	def parse(self, path, source, dest):
+		self.copy(path, source, dest)
+
+
+
