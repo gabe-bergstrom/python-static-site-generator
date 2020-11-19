72ddb3fad3da123aba945541f1275d3ef1faf565
gabe-bergstrom <gabe.bergstrom@reeher.com>
2020-11-19 10:02:09 -0600

    fixing import

--- ssg/site.py
+++ ssg/site.py
@@ -12,5 +12,5 @@ class Site:
 	def build(self):
 		self.dest.mkdir(parents = true, exist_ok = true)
 		for path in self.source.rglob("*"):
-			if(path.is_dir():
+			if(path.is_dir()):
 				self.create_dir(path)
